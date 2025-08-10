0784ecc6b7407efa0bfd900d0fe0a7fc57216665
Yiğit Şevki Kaplan <yigitkaplan2004@hotmail.com>
2025-08-10 17:49:30 +0300

    Chapter 1 (DEV-C++ ENVIRONMENT) completed

---
+++ CH1/EX1-7.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    printf("______________________________\n");
+    
+    printf("Name        Letter Grade\n");
+    
+    printf("______________________________\n");
+    
+    printf("Ahmet       BB\n");
+    printf("Fatma       DC\n");
+    printf("Ali         AA\n");
+    
+    printf("______________________________\n");
+    
+    system("PAUSE");
+    return(0);
+}
